59628bfecd30d1d7e4327a8b4c6b0740bb97763d
Latinovits Antal <latinovics.antal@gmail.com>
2018-10-06 21:41:41 +0200

    firstcommit

--- labyrinth/src/Main.java
+++ labyrinth/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("h");
+        readInput();
     }
+
+    private static Maze readInput() {
+        ArrayList<int[]> rows = new ArrayList<>();
+        Scanner reader = new Scanner(System.in);
+        while(reader.hasNextLine()) {
+            String[] str_elems = reader.nextLine().split(" ");
+            int[] elems = new int[str_elems.length];
+            for(int i = 0; i < str_elems.length; i++)
+                elems[i] = Integer.parseInt(str_elems[i]);
+            rows.add(elems);
+        }
+        int n_of_items = rows.remove(rows.size() - 1)[0];
+        return new Maze(rows, n_of_items);
+    }
+
 }
